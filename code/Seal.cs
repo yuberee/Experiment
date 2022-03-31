68f162169d8a93de5169a8c957205d654146f615
yuberee <59583743+yuberee@users.noreply.github.com>
2022-03-31 22:19:48 +0200

    Seal WIP + Animgraph change

---
+++ code/Seal.cs
@@ -0,0 +1,24 @@
+﻿using Sandbox;
+
+public partial class Seal : AnimEntity
+{
+
+	public override void Spawn()
+	{
+
+		base.Spawn();
+
+		SetModel( "models/seal/seal.vmdl" );
+		SetupPhysicsFromModel( PhysicsMotionType.Static );
+
+	}
+
+	[Event.Tick.Client]
+	public void Tick()
+	{
+
+		SetAnimParameter( "Honk", true );
+
+	}
+
+}
